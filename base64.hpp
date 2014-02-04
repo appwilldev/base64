513c7f5d4e255413558a06e54119ce229d88ffcc
Fengyuan Chen <cfy1990@gmail.com>
2014-02-04 21:04:27 +0800

    generalize base64 methods

--- base64.hpp
+++ base64.hpp
@@ -2,9 +2,178 @@
 #include <cstdlib>
 #include <memory>
 #include <tuple>
+#include <vector>
+#include <string>
 
 namespace base64{
-  using std::unique_ptr;
-  void encode(const char * src,size_t const src_l,unique_ptr<char []> & dst, size_t & dst_l);
-  void decode(const char * src,size_t const src_l,unique_ptr<char []> & dst,size_t & dst_l);
+const int unbase64_table[] =
+{ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2, -1, -1, -2, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, 62, -1, 63,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+    -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, 63,
+    -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+};
+inline int unbase64(size_t x){
+    return unbase64_table[x];
 }
+using std::unique_ptr;
+//void encode(const char * src,size_t const src_l,unique_ptr<char []> & dst, size_t & dst_l);
+//void decode(const char * src,size_t const src_l,unique_ptr<char []> & dst,size_t & dst_l);
+template<typename T1, typename T2>
+T2 encode(T1 src,size_t slen, T2 dst){
+    unsigned a;
+    unsigned b;
+    unsigned c;
+    unsigned i;
+    unsigned k;
+    unsigned n;
+
+    static const char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789+/";
+
+    i = 0;
+    k = 0;
+    n = slen / 3 * 3;
+
+    while (i < n) {
+        a = *src & 0xff;++src;
+        b = *src & 0xff;++src;
+        c = *src & 0xff;++src;
+
+        *dst = table[a >> 2];++dst;
+        *dst = table[((a & 3) << 4) | (b >> 4)];++dst;
+        *dst = table[((b & 0x0f) << 2) | (c >> 6)];++dst;
+        *dst = table[c & 0x3f];++dst;
+
+        i += 3;
+        k += 4;
+    }
+
+    if (n != slen) {
+        switch (slen - n) {
+            case 1:
+                a = *src & 0xff;
+                *dst = table[a >> 2];++dst;
+                *dst = table[(a & 0x3) << 4];++dst;
+                *dst = '=';++dst;
+                *dst = '=';++dst;
+            break;
+
+            case 2:
+                a = *src & 0xff;++src;
+                b = *src & 0xff;
+                *dst = table[a >> 2];++dst;
+                *dst = table[((a & 3) << 4) | (b >> 4)];++dst;
+                *dst = table[(b & 0x0f) << 2];++dst;
+                *dst = '=';++dst;
+            break;
+        }
+    }
+    return dst;
+}
+template<typename T1, typename T2>
+void decode(T1 src,T1 srcEnd, T2 dst){
+    char a,b,c,d;
+    while (src < srcEnd) {
+        int remaining = srcEnd - src;
+
+        while (unbase64(*src) < 0 && src < srcEnd) ++src, --remaining;
+        if (remaining == 0 || *src == '=') break;
+        a = unbase64(*src);++src;
+
+        while (unbase64(*src) < 0 && src < srcEnd) ++src, --remaining;
+        if (remaining <= 1 || *src == '=') break;
+        b = unbase64(*src);++src;
+
+        *dst++ = (a << 2) | ((b & 0x30) >> 4);
+
+        while (unbase64(*src) < 0 && src < srcEnd) ++src, --remaining;
+        if (remaining <= 2 || *src == '=') break;
+        c = unbase64(*src);++src;
+
+        *dst = ((b & 0x0F) << 4) | ((c & 0x3C) >> 2);++dst;
+
+        while (unbase64(*src) < 0 && src < srcEnd) ++src, remaining--;
+        if (remaining <= 3 || *src == '=') break;
+        d = unbase64(*src);++src;
+
+        *dst = ((c & 0x03) << 6) | (d & 0x3F);++dst;
+    }
+}
+//template<typename T1,typename T2>
+//void encode(T1 src,T1 src_end,T2 dst){
+    //while(true){
+        //size_t size=3;
+        //char buf[size];
+        //size_t i;
+        //for(i = 0; i < size && src != src_end; ++i,++src){
+            //buf[i] = *src;
+        //}
+        //dst = encode(buf, i, dst);
+        //if(src == src_end){
+            //break;
+        //}
+    //}
+//}
+
+inline size_t get_base64_encoded_size(size_t size){
+    return ((size + 2 - ((size + 2) % 3)) / 3 * 4);
+}
+inline size_t base64_decoded_size_fast_(size_t size) {
+    size_t remainder = size % 4;
+
+    size = (size / 4) * 3;
+    if (remainder) {
+        if (size == 0 && remainder == 1) {
+            // special case: 1-byte input cannot be decoded
+            size = 0;
+        } else {
+            // non-padded input, add 1 or 2 extra bytes
+            size += 1 + (remainder == 3);
+        }
+    }
+
+    return size;
+}
+template<typename T>
+size_t get_base64_decoded_size(T src, size_t size){
+    if (size == 0)
+        return 0;
+
+    if (src[size - 1] == '=')
+        size--;
+    if (size > 0 && src[size - 1] == '=')
+        size--;
+
+    return base64_decoded_size_fast_(size);
+}
+template<class T>
+using up = std::unique_ptr<T>;
+void encode(const char * src,size_t src_l, up<char []> & dst, size_t & dst_l);
+void decode(const char * src,size_t src_l, up<char []> & dst, size_t & dst_l);
+inline std::string encode(std::string const & src){
+    std::string dst;
+    dst.resize(get_base64_encoded_size(src.size()));
+    encode(src.begin(),src.size(),dst.begin());
+    return dst;
+}
+inline std::string decode(std::string const & src){
+    std::string dst;
+    dst.resize(get_base64_decoded_size(src.begin(),src.size()));
+    decode(src.begin(),src.end(),dst.begin());
+    return dst;
+}
+}
+
--- test.cc
+++ test.cc
@@ -2,15 +2,47 @@
 #include <cstring>
 #include <iostream>
 #include <string>
-#include <stdio.h>
-int main(int,char * argv[]){
-    std::unique_ptr<char []> foo;
-    size_t foo_l;
-    base64::encode(argv[1],strlen(argv[1]),foo,foo_l);
-    std::cout << std::string(foo.get(),foo_l) << "\n";
+#include <cstdio>
+#include <iterator>
+int main(int argc,char *argv[]){
+    if(argc == 2){
+        std::string tmp{base64::encode(argv[1])};
+        std::cout << base64::encode(std::string("aoeu")) << "\n";
+        std::cout << base64::decode(tmp) << "\n";
+        return 0;
+    }
+    if(std::string(argv[0]) == "./encode"){
+        size_t in_size = 1024*4*3;
+        char in_buf[in_size];
+        size_t out_size = base64::get_base64_encoded_size(in_size);
+        //char out_buf[out_size];
 
-    std::unique_ptr<char []> bar;
-    size_t bar_l;
-    base64::decode(foo.get(),foo_l,bar,bar_l);
-    std::cout << std::string(bar.get(),bar_l) << "\n";
+        while(true){
+            auto size_readed = std::fread((void *)in_buf,sizeof(in_buf[0]),in_size,stdin);
+            base64::up<char []> out{};
+            size_t out_size;
+            base64::encode(in_buf,size_readed,out,out_size);
+            std::fwrite(static_cast<void *>(out.get()),sizeof(*out.get()),out_size,stdout);
+            //base64::encode(in_buf,size_readed,out_buf);
+            //std::fwrite(static_cast<void*>(out_buf),sizeof(out_buf[0]),base64::get_base64_encoded_size(size_readed),stdout);
+            assert(size_readed<=in_size);
+            if(size_readed<in_size)
+                break;
+        }
+    }else{
+        size_t in_size = 1024*4;
+        char in_buf[in_size];
+        size_t out_size = in_size / 4 * 3;
+        char out_buf[out_size];
+        while(true){
+            auto size_readed = std::fread((void *)in_buf,sizeof(in_buf[0]),in_size,stdin);
+            while(in_buf[size_readed - 1] == '\n')
+                --size_readed;
+            base64::decode(in_buf,in_buf + size_readed,out_buf);
+            std::fwrite((void *)out_buf,sizeof(out_buf[0]),base64::get_base64_decoded_size(in_buf,size_readed),stdout);
+            assert(size_readed<=in_size);
+            if(size_readed<in_size)
+                break;
+        }
+    }
 }
